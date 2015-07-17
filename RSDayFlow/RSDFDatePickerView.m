260abb18c0ac1efed44e5bbfb315ae47e62d4958
Christian Hatch <christianhatch@gmail.com>
2015-07-17 14:42:40 -0400

    Fixed a deprecation warning.

--- RSDayFlow/RSDFDatePickerView.m
+++ RSDayFlow/RSDFDatePickerView.m
@@ -355,7 +355,7 @@ static NSString * const RSDFDatePickerViewDayCellIdentifier = @"RSDFDatePickerVi
 
 - (NSDate *)dateWithoutTimeComponents:(NSDate *)date
 {
-    NSDateComponents *dateComponents = [self.calendar components:NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit fromDate:date];
+    NSDateComponents *dateComponents = [self.calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:date];
     return [self.calendar dateFromComponents:dateComponents];
 }
 
